872c867d6398909b6754f02ab8231be4e2ff6bc7
Sara Martinez Soto <saramartinez769@u.boisestate.edu>
2024-10-16 13:33:20 -0600

    Defining EvilCircle

--- js/main.js
+++ js/main.js
@@ -28,6 +28,31 @@ class Shape {
   }
 }
 
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = white;
+    this.size = 10;
+    window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+      case "a":
+        this.x -= this.velX;
+        break;
+      case "d":
+        this.x += this.velX;
+        break;
+      case "w":
+        this.y -= this.velY;
+        break;
+      case "s":
+        this.y += this.velY;
+        break;
+    }
+  });
+  }
+  
+}
+
 class Ball extends Shape{
 
   constructor(x, y, velX, velY, color, size) {
